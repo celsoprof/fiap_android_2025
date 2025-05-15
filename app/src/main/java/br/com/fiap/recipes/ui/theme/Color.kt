f49020cd48e513760c46f937cd3beafcf348d36b
Celso Furtado <celsofurtado@celsomacbook.local>
2025-05-15 17:11:17 -0300

    Capítulo 03

--- app/src/main/java/br/com/fiap/recipes/ui/theme/Color.kt
+++ app/src/main/java/br/com/fiap/recipes/ui/theme/Color.kt
@@ -9,3 +9,15 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// CORES DO TEMA CLARO
+val LightPrimary = Color(0xFFED1459) // Vermelho vibrante dos cards
+val LightSecondary = Color(0xFF808080) // Cinza para texto secundário
+val LightBackground = Color(0xFFFFFFFF) // Branco para o fundo
+val LightSurface = Color(0xFFFFFFFF) // Branco para superfícies
+val LightOnPrimary = Color(0xFFFFFFFF) // Texto sobre o primário
+val LightOnSecondary = Color(0xFFFFFFFF) // Texto sobre o secundário
+val LightOnBackground = Color(0xFF000000) // Texto sobre o fundo
+val LightOnSurface = Color(0xFF000000) // Texto sobre a superfície (preto)
+val LightTertiary = Color(0xFFFFD740) // Amarelo da barra de navegação
+val LightOnTertiary = Color(0xFF000000) // Texto/ícones sobre o amarelo
