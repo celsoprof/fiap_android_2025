f48dc9fd0311db31528145693ffda31b3ac775cd
Prof Celso <celso.furtado@sp.senai.br>
2025-06-27 16:36:04 -0300

    Iniciando configuração NavHost

--- app/src/main/java/br/com/fiap/recipes/MainActivity.kt
+++ app/src/main/java/br/com/fiap/recipes/MainActivity.kt
@@ -3,6 +3,7 @@ package br.com.fiap.recipes
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import br.com.fiap.recipes.navigation.NavigationRoutes
 import br.com.fiap.recipes.screens.HomeScreen
 import br.com.fiap.recipes.screens.LoginScreen
 import br.com.fiap.recipes.screens.SignupScreen
@@ -14,10 +15,7 @@ class MainActivity : ComponentActivity() {
         //enableEdgeToEdge()
         setContent {
             RecipesTheme {
-                //InitialScreen()
-                //SignupScreen()
-                //LoginScreen()
-                HomeScreen()
+                NavigationRoutes()
             }
         }
     }
--- app/src/main/java/br/com/fiap/recipes/navigation/NavigationRoutes.kt
+++ app/src/main/java/br/com/fiap/recipes/navigation/NavigationRoutes.kt
@@ -1,2 +1,20 @@
 package br.com.fiap.recipes.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import br.com.fiap.recipes.screens.InitialScreen
+import br.com.fiap.recipes.screens.LoginScreen
+
+@Composable
+fun NavigationRoutes() {
+    val navController = rememberNavController();
+    NavHost(
+        navController = navController,
+        startDestination = InitialScreenRoute
+    ){
+        composable<LoginScreenRoute>{ LoginScreen() }
+        composable<InitialScreenRoute>{ InitialScreen() }
+    }
+}
