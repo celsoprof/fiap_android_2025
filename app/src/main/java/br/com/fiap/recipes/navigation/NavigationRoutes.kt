3d28cf85f890a8dad58b6800018a8e50313c1dec
Celso Furtado <celsofurtado@Celsos-Mac-mini.local>
2025-07-01 17:53:34 -0300

    Capítulo 05 concluído

--- app/src/main/java/br/com/fiap/recipes/navigation/NavigationRoutes.kt
+++ app/src/main/java/br/com/fiap/recipes/navigation/NavigationRoutes.kt
@@ -1,11 +1,13 @@
 package br.com.fiap.recipes.navigation
 
+import android.content.Intent
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import androidx.navigation.navDeepLink
 import br.com.fiap.recipes.screens.HomeScreen
 import br.com.fiap.recipes.screens.InitialScreen
 import br.com.fiap.recipes.screens.LoginScreen
@@ -18,17 +20,27 @@ fun NavigationRoutes() {
         navController = navController,
         startDestination = Destination.InitialScreen.route
     ) {
-        composable(Destination.InitialScreen.route){ InitialScreen(navController) }
+        composable(Destination.InitialScreen.route) { InitialScreen(navController) }
 
         composable(
             route = Destination.HomeScreen.route,
-            arguments = listOf(navArgument("email") { type = NavType.StringType })
-        ){ backStackEntry ->
+            deepLinks = listOf(
+                navDeepLink {
+                    uriPattern = "https://recipes.fiap.com.br/email/{email}"
+                    action = Intent.ACTION_VIEW
+                }
+            ),
+            arguments = listOf(
+                navArgument("email") {
+                    type = NavType.StringType
+                }
+            )
+        ) { backStackEntry ->
             var email = backStackEntry.arguments?.getString("email")
             HomeScreen(email!!)
         }
 
         composable(Destination.SignupScreen.route) { SignupScreen() }
-        composable(Destination.LoginScreen.route) { LoginScreen(navController)}
+        composable(Destination.LoginScreen.route) { LoginScreen(navController) }
     }
 }
