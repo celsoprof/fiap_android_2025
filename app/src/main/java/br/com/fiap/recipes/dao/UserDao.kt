bcc15d2d67e55a44c06f42a458a82b8d47146e17
Celso Furtado <celsofurtado@celsomacbook.local>
2025-08-07 12:03:52 -0300

    Preparação Room

---
+++ app/src/main/java/br/com/fiap/recipes/dao/UserDao.kt
@@ -0,0 +1,26 @@
+package br.com.fiap.recipes.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import br.com.fiap.recipes.model.User
+
+@Dao
+interface UserDao {
+
+    @Insert
+    fun save(user: User): Int
+
+    @Delete
+    fun delete(user: User): Int
+
+    @Update
+    fun update(user: User): Int
+
+    @Query("SELECT * FROM tb_user WHERE " +
+            "email = :email AND password = :password LIMIT 1")
+    fun login(email: String, password: String): User?
+
+}
--- app/src/main/java/br/com/fiap/recipes/model/User.kt
+++ app/src/main/java/br/com/fiap/recipes/model/User.kt
@@ -1,8 +1,14 @@
 package br.com.fiap.recipes.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tb_user")
 data class User(
-    val id: Int = 0,
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val name: String = "",
     val email: String = "",
-    val password: String = ""
+    val password: String = "",
+    @ColumnInfo(name = "user_image") val userImage: ByteArray?
 )
